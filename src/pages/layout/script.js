faa4ba45e766c2015ff2b3a226e34a021e57f4b6
raywrk <ray.gabriel18@gmail.com>
2022-05-31 21:40:45 -0300

    feature/painel-list-component

--- src/pages/layout/script.js
+++ src/pages/layout/script.js
@@ -1,11 +1,91 @@
 import { painelComponent } from "../painel/painel.js"
+import { icons } from "../../data/icons.js"
+import { padTo2Digits, formatDate } from "../../data/date.js"
+import { Categoria } from "../../models/category.js"
 
 const $container = document.querySelector('.container')
-$container.innerHTML = painelComponent
-const $categoriasAdd = document.querySelector('.categorias-add img')
+$container.innerHTML = painelComponent()
 
-$categoriasAdd.addEventListener('click', nome)
+const $btnCategoriasAdd = document.querySelector('.categorias-add img')
+const $modal = document.getElementById('myModal');
+const $span = document.getElementsByClassName('close')[0];
+const $renderIcon = document.querySelector('.modal-body-img')
+const $selectIcon = document.querySelector('.iconSelect')
+const $selectColorIcon = document.querySelector('.iconColorSelect')
 
-function nome(){
-  console.log('ALOO')
+// Inputs do Modal
+
+const $categoriaTitle = document.querySelector('.categoriaTitle')
+const $categoriaDescricao = document.querySelector('.categoriaDescricao')
+const $addCategory = document.querySelector('.salvar')
+const $renderCategoryItens = document.querySelector('.categorias-cards-container')
+const CategoryArray = JSON.parse(localStorage.getItem('ListCategorys')) ?? []
+RenderCategory()
+
+if($selectIcon || $selectColorIcon || $addCategory){
+  $selectIcon.addEventListener('change', iconChange)
+  $selectColorIcon.addEventListener('change', iconChange)
+  $addCategory.addEventListener('click', addCategory)
+}
+
+function iconChange(){
+  let iconName = ''
+  let iconSelected
+  iconName = $selectIcon.value + '-' + $selectColorIcon.value
+  iconSelected = icons.find((icon) => icon.name == iconName)
+  $renderIcon.innerHTML = `<img class="iconSrc" src="${iconSelected.src}">`
+}
+
+$btnCategoriasAdd.onclick = function(){
+  $modal.style.display = 'block'
+}
+
+$span.onclick = function(){
+  $modal.style.display = 'none'
+}
+
+window.onclick = function(event) {
+  if (event.target == $modal) {
+    $modal.style.display = "none";
+  }
+}
+
+function addCategory(){
+
+  const id = Object.keys(CategoryArray).length + 1
+  const title = $categoriaTitle.value
+  let iconName = ''
+  let iconSelected
+  iconName = $selectIcon.value + '-' + $selectColorIcon.value
+  iconSelected = icons.find((icon) => icon.name == iconName)
+  const icon = iconSelected.src
+  const description = $categoriaDescricao.value
+  const created_at = formatDate(new Date())
+  const status = true
+  const task = []
+  let novaCategoria = new Categoria(id, title, icon, description, created_at, status, task)
+  CategoryArray.push(novaCategoria)
+  localStorage.setItem('ListCategorys', JSON.stringify(CategoryArray));
+  RenderCategory()
+  clear()
+}
+
+function clear(){
+  
+  $categoriaTitle.value = ''
+  $categoriaDescricao.value = ''
+  
+}
+
+function RenderCategory(){
+  let categoryItens = ''
+  CategoryArray.forEach((element) => {  
+    categoryItens += `
+    <div onClick="openList(${element.id})" class="categorias-cards">
+      <img src="${element.icon}" alt="">
+      <p>${element.title}</p>
+    </div>
+    `
+    $renderCategoryItens.innerHTML = categoryItens
+  })
 }
--- src/pages/painel/painel.js
+++ src/pages/painel/painel.js
@@ -1,46 +1,58 @@
-export const painelComponent = `
-
+export const painelComponent = () => {
+  return `
 <section class="container"> 
+
 <div class="categorias-add">
   <img src="../../assets/plus.svg" alt="">
+  <p>Adicionar Categoria</p>
+</div>
+
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+    </div>
+    <div class="modal-body">
+      <p>Nome da Categoria</p>
+      <input class="categoriaTitle" type="text">
+      <p>Icone da Categoria</p>
+      <select name="icons" class="iconSelect">
+        <option value="Selecione">Selecione</option>
+        <option value="casa">Casa</option>
+        <option value="escola">Escola</option>
+        <option value="compras">Compras</option>
+        <option value="passeio">Passeio</option>
+      </select>
+    
+      <div class="modal-body-icon">
+        <div class="modal-body-img"></div>
+        <div>
+          <p>Cor do icone</p>
+          <select name="colors" class="iconColorSelect">
+            <option value="azul">Azul</option>
+            <option value="rosa">Rosa</option>
+            <option value="roxo">Roxo</option>
+            <option value="vermelho">Vermelho</option>
+          </select>
+        </div>
+      </div>
+
+      <p>Descrição</p>
+      <textarea class="categoriaDescricao" type="text"></textarea>
+
+      <input type="button" value="Salvar" class="salvar">  
+
+    </div>
+  </div>
+
 </div>
 
 <div class="categorias-cards-container">
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
-  <div class="categorias-cards">
-    <img src="../../assets/map_gym.svg" alt="">
-    <p>Academia</p>
-  </div>
+  
 </div>
 </section>
 
 `
-
-
+}
